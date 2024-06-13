3168d99a18a4726ffa9ba2bbe886e740fab4e1d5
Oni <OniLin@universalec.com>
2024-06-13 12:27:07 +0800

    直接使用 MVC ASP 吧

---
+++ CH2-1/Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH2_1
+{
+    // 這邊  internal 可以不寫，預設就是internal可見而已。
+    internal class Hello
+    {
+        public static void Main()
+        {
+            // 新增物件方式 可以化簡為 Bike b = new();
+            String hello = "Hello, Oni";
+            Console.WriteLine(hello);
+        }
+    }
+}
